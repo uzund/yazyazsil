5387cc0bb771ce4a3ac838c73d4ca079a64adee2
Davut Uzun <duzun@k1m41s05.42istanbul.com.tr>
2022-12-23 00:37:48 +0300

    Update..

--- lcm.c
+++ lcm.c
@@ -1,7 +1,19 @@
-#include <unistd.h>
-//#include <stdio.h>
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   lcm.c                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: duzun <davut@uzun.ist>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/23 00:34:35 by duzun             #+#    #+#             */
+/*   Updated: 2022/12/23 00:37:16 by duzun            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-int hcf(int a, int b)
+#include <unistd.h>
+// #include <stdio.h>
+
+int	hcf(int a, int b)
 {
 	if (b == 0)
 		return (a);
@@ -11,7 +23,7 @@ int hcf(int a, int b)
 
 unsigned int	lcm(unsigned int a, unsigned int b)
 {
-	if (a <= 0 || b <= 0)
+	if ((int)a <= 0 || (int)b <= 0)
 	{
 		write(1, "0\n", 2);
 		return (0);
@@ -21,8 +33,10 @@ unsigned int	lcm(unsigned int a, unsigned int b)
 /*
 int	main(void)
 {
+	int	a;
+	int	b;
+
 	printf("En Küçük Ortak Katını (EKOK) bulmak için iki pozitif sayı girin \n");
-	int a, b;
 	scanf("%d %d", &a, &b);
 	printf("%d ve  %d sayılarının EKOK değeri:  %d\n", a, b, lcm(a, b));
 	return (0);
